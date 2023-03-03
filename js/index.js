3683f49abc6522a213fb35b0256a84b9d1b29e23
jihanstkm <jihanstk@gmail.com>
2023-03-03 13:24:57 +0600

    data fetch and load

---
+++ js/index.js
@@ -0,0 +1,57 @@
+
+const loadDataFromApi =()=>{
+
+    fetch("https://openapi.programming-hero.com/api/ai/tools")
+    .then(res=>res.json())
+    .then(data=>showData(data.data.tools))
+
+
+}
+
+
+const showData =infos=>{
+    console.log(infos);
+    
+    const cardContainer = document.getElementById("card-container");
+
+   
+    infos.forEach(info => {
+        let colElement =document.createElement("div");
+        
+        colElement.classList.add("col");
+        colElement.innerHTML= `
+        <div class="card p-4">
+        <img src="${info.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">features</h5>
+        <ol id="ol-e">
+        <li>${info.features[0]}</li>
+        <li>  ${info.features[1]}</li>
+        <li> ${info.features[2]}</li>
+        </ol>
+        <hr>
+        <div id="footer" class="footer d-flex justify-content-between align-items-center">
+        <div class="text">
+        <h5>${info.name}</h5>
+        <p> <i class="fa-regular fa-calendar-days"></i> <span>${info.published_in}</span></p>
+        </div>
+        <div class="text">
+        <button onclick="" id="detais-btn"> <i class="fa-solid fa-arrow-right fs-4"></i></button>
+        </div> 
+        </div>
+        </div>
+        </div>
+    
+        `
+        
+        cardContainer.appendChild(colElement)
+    });
+   
+  
+}
+
+
+
+
+
+loadDataFromApi()
