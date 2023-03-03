6c4dddc482d6f6780c03d8b131447f8170333f86
jihanstkm <jihanstk@gmail.com>
2023-03-03 17:45:37 +0600

    add modal

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-
+// from api data load
 const loadDataFromApi =(dataLimit)=>{
     loader(true)
     fetch("https://openapi.programming-hero.com/api/ai/tools")
@@ -8,7 +8,7 @@ const loadDataFromApi =(dataLimit)=>{
 
 }
 
-
+// displayed data
 const showData =(infos,dataLimit)=>{
     console.log(infos);
     
@@ -42,7 +42,9 @@ const showData =(infos,dataLimit)=>{
         <p> <i class="fa-regular fa-calendar-days"></i> <span>${info.published_in}</span></p>
         </div>
         <div class="text">
-        <button onclick="" id="detais-btn"> <i class="fa-solid fa-arrow-right fs-4"></i></button>
+        <button onclick="loadDetailsData('${info.id}')" type="button" id="details-btn" data-bs-toggle="modal" href="#aiHubModal" role="button">
+        <i class="fa-solid fa-arrow-right fs-4"></i>
+         </button>
         </div> 
         </div>
         </div>
@@ -58,7 +60,7 @@ const showData =(infos,dataLimit)=>{
 
 
 
-
+// added spinner
 const loader=isLoading =>{
     const loaderSection = document.getElementById("loader");
     if(isLoading){
@@ -68,7 +70,7 @@ const loader=isLoading =>{
     }
 }
 
-
+//if > 6 dataload 
 document.getElementById("show-btn").addEventListener("click",()=>{
 
 loadDataFromApi()
@@ -76,5 +78,66 @@ loadDataFromApi()
 })
 
 
+const loadDetailsData=(id)=>{
+    const url =`https://openapi.programming-hero.com/api/ai/tool/${id}`
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>detailsDisplay(data.data))
+}
+
+const detailsDisplay =(details)=>{
+    
+    console.log(details);
+    const modalContainer = document.getElementById("modal-container");
+    
+    modalContainer.innerHTML=`
+            <div class="modal-content">
+            <div class="modal-header">
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body d-flex justify-content-between">
+            <div class="row">
+                <div class="col-md-6">
+                    <h4>${details.description}</h4>
+                    <div class="row">
+                    <div class="col-md-4 p-3">
+                    <p class=" text-success">${details.pricing ? details.pricing[0].price : "not found"} ${details.pricing? details.pricing[0].plan :"not Found"}</p>
+                    </div>
+                    <div class="col-md-4 p-3"><p class="text-warning">${details.pricing? details.pricing[1].price:"not found"} ${details.pricing? details.pricing[1].plan :"not Found"}</p></div>
+                    <div class="col-md-4 p-3"><p class="text-danger">${details.pricing[2].price} ${details.pricing[2].plan}</p></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-6">
+                            <h5>feature</h5>
+                            <ul>
+                               
+                            </ul>
+                        </div>
+                        <div class="col-6">
+                            <h5>integration</h5>
+                            <ul>
+                              
+                            </ul>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="col-6">
+                        
+                        <div class="image">
+                           
+                        </div>
+
+                        <div class="text-center mt-5">
+                           
+                        </div>
+
+                    </div>
+            </div>
+            </div>
+            </div>
+    `;
+
+
+}
 
 loadDataFromApi(6)
